35d5f5b41e9834a74421380e26531dbf8563bd04
Beata <beatarudzka89@gmail.com>
2022-09-06 20:35:26 +0200

    plus one - leetcode

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import { LinkedList } from "./data.js";
+//https://leetcode.com/problems/plus-one/
 
-let myLinkedList = new LinkedList(7);
-myLinkedList.push(4);
-console.log(myLinkedList);
+let digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+const plusOne = function (digits) {
+  let numbers = String(digits.join(""));
+  numbers = BigInt(numbers);
+  numbers++;
+  const numbArr = String(numbers)
+    .split("")
+    .map((el) => {
+      return Number(el);
+    });
+  return numbArr;
+};
+
+let digitsPlusOne = plusOne(digits);
