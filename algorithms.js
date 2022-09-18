b39f7a419b8d6c13db9b6fc3f9d7bed5542ea937
Beata <beatarudzka89@gmail.com>
2022-09-18 15:15:45 +0200

    maxSubArray

--- algorithms.js
+++ algorithms.js
@@ -213,3 +213,15 @@ var containsDuplicate = function (nums) {
 };
 
 const result = containsDuplicate([1, 1, 2, 0, 3]);
+
+const maxSubArray = function (nums) {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(currentSum + nums[i], nums[i]);
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  return maxSum;
+};
+
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
